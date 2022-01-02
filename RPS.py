d99fd4e326f3a1e5c8926339924072824e8fdda7
FirestormFan / SRPN <77221405+Firestormfan@users.noreply.github.com>
2022-01-03 01:54:36 +0900

    Update RPS.py
    ・小文字に対応
    ・Eで抜けれるように

--- RPS.py
+++ RPS.py
@@ -1,10 +1,20 @@
 import random
+
+weapons = ["R", "P", "S"]
+
+def main():
+	pdf = input("どれか選んでね (R: グー, P: パー, S: チョキ) 終了するときは(e): ").upper()
+	if pdf == "E":
+		print("終了します")
+		exit()
+	if pdf in weapons:
+		tsp = random.choice(weapons)
+		if pdf == tsp:
+			print ("あいこ")
+		elif (pdf == "R" and tsp == "P") or (pdf == "P" and tsp == "S") or (pdf == "S" and tsp == "R"):
+			print("負け")
+		elif (pdf == "P" and tsp == "R") or (pdf == "R" and tsp == "S") or (pdf == "S" and tsp == "P"):
+			print("勝ち")
+
 while True:
-	pdf = input("どれか選んでね (R, P, S) 終了するときは(e)")
-	tsp = random.select(["R","P","S"])
-	if pdf == tsp:
-		print ("あいこ")
-	elif (pdf == "R" and tsp == "P") or (pdf == "P" and tsp == "S") or (pdf == "S" and tsp == "R"):
-		print("負け")
-	elif (pdf == "P" and tsp == "R") or (pdf == "R" and tsp == "S") or (pdf == "S" and tsp == "P"):
-		print("勝ち")
+	main()
